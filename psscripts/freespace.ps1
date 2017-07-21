5aac9dda396855397bc32e7e881a1b44e45f0f99
O.T Osman <sirrush.oo@gmail.com>
2017-07-21 05:51:19 -0700

    Add files via upload

---
+++ psscripts/freespace.ps1
@@ -0,0 +1,8 @@
+﻿[CmdletBinding()]
+
+param (
+    [Parameter(Mandatory=$True)]
+
+    [string[]]$computername
+)
+Get-WmiObject -ComputerName $computername -class win32_logicaldisk -Filter "DeviceID='C:'"
