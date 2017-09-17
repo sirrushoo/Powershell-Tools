f02ec94aacc096cc283b7d747032011d1514e182
O.T Osman <sirrush.oo@gmail.com>
2017-09-17 02:46:27 -0700

    Add files via upload

---
+++ psscripts/Set-TimeStamp.ps1
@@ -0,0 +1,31 @@
+﻿# Changing TimeStamps on files 
+#
+# You must edit the file to suit your file
+#
+#Get-ChildItem  C:\Directiory\File.txt | % {$_.CreationTime = '01/11/2005 06:00:36'}
+#Set the time in the "$date" variable to when you changed the log or files
+#
+
+Function Set-FileTimeStamps
+
+{
+
+ Param (
+
+    [Parameter(mandatory=$true)]
+
+    [string[]]$path,
+
+    [datetime]$date = (Get-Date))
+
+    Get-ChildItem -Path $path |
+
+    ForEach-Object {
+
+     $_.CreationTime = $date
+
+     $_.LastAccessTime = $date
+
+     $_.LastWriteTime = $date }
+
+} #end function Set-FileTimeStamps
