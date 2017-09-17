cd2b21f9481007cb319adc2604194304c2582dee
O.T Osman <sirrush.oo@gmail.com>
2017-09-17 02:49:37 -0700

    Update Set-TimeStamp.ps1

--- psscripts/Set-TimeStamp.ps1
+++ psscripts/Set-TimeStamp.ps1
@@ -1,9 +1,9 @@
-﻿# Changing TimeStamps on files 
+# Changing TimeStamps on files 
 #
 # You must edit the file to suit your file
 #
-#Get-ChildItem  C:\Directiory\File.txt | % {$_.CreationTime = '01/11/2005 06:00:36'}
-#Set the time in the "$date" variable to when you changed the log or files
+# Get-ChildItem  C:\Directiory\File.txt | % {$_.CreationTime = '01/11/2005 06:00:36'}
+# Set the time in the "$date = '01/11/2005 06:00:36' " variable to when you changed the log or files
 #
 
 Function Set-FileTimeStamps
